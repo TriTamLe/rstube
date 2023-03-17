450b77c48b3fc31363407ae666169892a11ea20e
TriTamLe <tritam.le54@gmail.com>
2023-03-17 17:59:52 +0700

    update

--- src/resources/components/Add&Delete/Add.js
+++ src/resources/components/Add&Delete/Add.js
@@ -24,6 +24,7 @@ function Add({ openCallback, addCallback, allchannels }) {
   const inputChange = e => {
     console.log('CHANGE');
     setInputText(e.target.value);
+    console.log(inputText);
   };
 
   const exitAdding = () => {
